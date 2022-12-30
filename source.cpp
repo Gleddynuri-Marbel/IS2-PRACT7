8f2fa1f51f25fa53b6acfed411c3ac1f0a238814
Gleddynuri-Marbel <86405228+Gleddynuri-Marbel@users.noreply.github.com>
2022-12-30 09:24:34 -0500

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,100 @@
+class Persona {
+   String nombre;
+   String dni;
+   String codArea;
+
+
+   String
+numTelefono;
+
+
+  
+
+
+  String
+getNombre() {
+
+
+    return
+nombre;
+
+
+  }
+
+
+  String getDni()
+{
+
+
+    return dni;
+
+
+  }
+
+
+  String getNumeroTelefono() {
+
+
+    return codArea
++ numTelefono;
+
+
+  }
+
+
+}
+
+class Empresa {
+   String
+nombre;
+   String ruc;
+   String codArea;
+   String
+numTelefono;
+  
+  String
+getNombre() {
+    return
+nombre;
+  }
+  String getRuc()
+{
+    return ruc;
+  }
+  String
+getNumeroTelefono() {
+    return
+codArea + numTelefono;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
